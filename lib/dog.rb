34f5b8764b73ef7de0ccc2d79d96b6a2fb60b472
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-22 19:19:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,9 @@
 class Dog 
   
-  def initialize 
+  def initialize =(first_name, last_name)
+    @first_name = first_name 
+    @last_name = last_name 
+  end 
+  
+  def initialize = (breed)
+    
